ea54668ffac0545d57b95fc5adea4fafa612c73b
Olena Shedenko <mea612@gmail.com>
2020-10-18 17:25:27 +0300

    change switch to if

--- basic-javascript/homework1_alert-prompt-confirm/js/script.js
+++ basic-javascript/homework1_alert-prompt-confirm/js/script.js
@@ -6,17 +6,32 @@ while(!name || !isNaN(name) || isNaN(age)) {
     age = +prompt("Enter your age again, please", age);
 }
 
-switch (true) {
-    case age < 18: 
-        alert("You are not allowed to visit this website.");
-        break;
-    case age <= 22: 
-        if(confirm("Are you sure you want to continue?")) {
-            alert(`Welcome, ${name}`); 
-        } else {
-            alert("You are not allowed to visit this website"); 
-        }     
-        break;
-    default: 
-        alert(`Welcome, ${name}`);
+if(age < 18) {
+    alert("You are not allowed to visit this website.");
+} else if(age <= 22) {
+    if(confirm("Are you sure you want to continue?")) {
+        alert(`Welcome, ${name}`); 
+    } else {
+        alert("You are not allowed to visit this website"); 
+    }  
+} else {
+    alert(`Welcome, ${name}`);
 }
+
+
+
+
+// switch (true) {
+//     case age < 18: 
+//         alert("You are not allowed to visit this website.");
+//         break;
+//     case age <= 22: 
+//         if(confirm("Are you sure you want to continue?")) {
+//             alert(`Welcome, ${name}`); 
+//         } else {
+//             alert("You are not allowed to visit this website"); 
+//         }     
+//         break;
+//     default: 
+//         alert(`Welcome, ${name}`);
+// }
