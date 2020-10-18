33e4202c82a15d20094eb769bfa2430039f6a807
Olena Shedenko <mea612@gmail.com>
2020-10-18 21:14:21 +0300

    add one more solution in comments

--- basic-javascript/homework2_find-multiples/js/script.js
+++ basic-javascript/homework2_find-multiples/js/script.js
@@ -4,10 +4,10 @@ while(!Number.isInteger(num)){
 	
 }
 
-if(num < 5 || num % 5 != 0) {
+if(num < 5) { // no (|| num % 5 != 0)
 	console.log('Sorry, no numbers');
 } else {
-	for(let i = 1; i <= num; i++) {
+	for(let i = 1; i <= num; i++) { //  One more solution for(let i = 5; i <= num; i+5) { console.log(i);}
 		if(i % 5 == 0) {
 			console.log(i);
 		}
