111df9060c7bb0ed2da195359a6fb60f5de8d47e
Olena Shedenko <mea612@gmail.com>
2020-11-24 19:03:44 +0200

    fix code review

--- basic-javascript/homework12_cycled-slider/js/script.js
+++ basic-javascript/homework12_cycled-slider/js/script.js
@@ -1,32 +1,62 @@
-const slides = document.querySelectorAll('.image-to-show');
-let currentSlide = 0;
-let isSliderActive = true;
+// const slides = document.querySelectorAll('.image-to-show');
+// let currentSlide = 0;
+// let isSliderActive = true;
+
+// function showNextImage() {
+//     if(!isSliderActive) return;
+//     slides[currentSlide].classList.remove('image-visible');
+//     setTimeout(() => {
+//         slides[currentSlide].style.display = 'none';
+//         currentSlide++;
+//         if(currentSlide === slides.length) {
+//             currentSlide = 0;
+//         }
+//         slides[currentSlide].classList.add('image-visible');
+//         slides[currentSlide].style.display = 'block';
+//     }, 500)
+// }
+
+
+// slides[0].style.display = 'block';
+// setInterval(showNextImage, 3000);
+
+// document.querySelector('.pauseBtn').addEventListener('click', () => {
+//     isSliderActive = false;
+// });
+
+// document.querySelector('.continueBtn').addEventListener('click', () => {
+//     isSliderActive = true;
+// });
+
+let intervalId;
+const slides = Array.from(document.querySelectorAll('.image-to-show'));
 
 function showNextImage() {
-    if(!isSliderActive) return;
+    const currentSlide = slides.indexOf(document.querySelector('.image-visible'));
     slides[currentSlide].classList.remove('image-visible');
     setTimeout(() => {
         slides[currentSlide].style.display = 'none';
-        currentSlide++;
-        if(currentSlide === slides.length) {
-            currentSlide = 0;
-        }
-        slides[currentSlide].classList.add('image-visible');
-        slides[currentSlide].style.display = 'block';
+        const nextSlide = currentSlide === slides.length - 1 ? 0 : currentSlide + 1;
+        slides[nextSlide].classList.add('image-visible');
+        slides[nextSlide].style.display = 'block';
     }, 500)
 }
-
-
 slides[0].style.display = 'block';
-setInterval(showNextImage, 3000);
+
+function startImageSwitch() {
+    intervalId = setInterval(showNextImage, 3000);
+}
+
+function stopImageSwitch() {
+    clearInterval(intervalId);
+}
+
+startImageSwitch();
 
 document.querySelector('.pauseBtn').addEventListener('click', () => {
-    isSliderActive = false;
+    stopImageSwitch();
 });
 
 document.querySelector('.continueBtn').addEventListener('click', () => {
-    isSliderActive = true;
+    startImageSwitch();
 });
-
-
-
