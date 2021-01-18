465d3c8fc80447dcad34583ee3b12f6e7b54b202
Olena Shedenko <mea612@gmail.com>
2021-01-18 13:02:32 +0200

    fix code review

--- advanced-javascript/homework2-try-catch/js/script.js
+++ advanced-javascript/homework2-try-catch/js/script.js
@@ -30,33 +30,68 @@ const books = [
 
 function createBooksList(booksList) {
     let bookList = '';
-    for(let i = 0; i < booksList.length; i++) {
-        
+    booksList.forEach(item => {
         try {
-            const author = getProperty(booksList[i], 'author');
-            const name = getProperty(booksList[i], 'name');
-            const price = getProperty(booksList[i], 'price');
-
-            bookList += `<ul>${createBookEntry(author, name, price)}</ul>`;
-
+            bookList += `<ul>${createBookEntry(item)}</ul>`;
         } catch(e) {
             console.error(e.message);
         }
-    
-    }
+
+    });
     const parent = document.getElementById('root');
     parent.innerHTML = bookList;
-} 
 
-function getProperty(obj, prop) {
-    if(obj[prop]) {
-        return obj[prop];
-    } else {
-        throw new Error(`The property ${prop} is not defined`);
-    } 
 }
 
-const createBookEntry = (author, name, price) =>
-    `<li>${author}</li><li>${name}</li><li>${price}</li>`;
+function createBookEntry(item) {
+    let bookItem = '';
+    if(item.hasOwnProperty("author")) {
+        bookItem += `<li>${item.author}</li>`
+    } else {
+        throw new Error(`The property "author" is missing in ${JSON.stringify(item)}`);
+    }
+    if(item.hasOwnProperty("name")) {
+        bookItem += `<li>${item.name}</li>`
+    } else {
+        throw new Error(`The property "name" is missing in ${JSON.stringify(item)}`);
+    }
+    if(item.hasOwnProperty("price")) {
+        bookItem += `<li>${item.price}</li>`
+    } else {
+        throw new Error(`The property "price" is missing in ${JSON.stringify(item)}`);
+    }
+    return bookItem;
+}
+
+// function createBooksList(booksList) {
+//     let bookList = '';
+//     for(let i = 0; i < booksList.length; i++) {
+        
+//         try {
+//             const author = getProperty(booksList[i], 'author');
+//             const name = getProperty(booksList[i], 'name');
+//             const price = getProperty(booksList[i], 'price');
+
+//             bookList += `<ul>${createBookEntry(author, name, price)}</ul>`;
+
+//         } catch(e) {
+//             console.error(e.message);
+//         }
+    
+//     }
+//     const parent = document.getElementById('root');
+//     parent.innerHTML = bookList;
+// } 
+
+// function getProperty(obj, prop) {
+//     if(obj[prop]) {
+//         return obj[prop];
+//     } else {
+//         throw new Error(`The property ${prop} is not defined in ${JSON.stringify(obj)}`);
+//     } 
+// }
+
+// const createBookEntry = (author, name, price) =>
+//     `<li>${author}</li><li>${name}</li><li>${price}</li>`;
 
 createBooksList(books);
